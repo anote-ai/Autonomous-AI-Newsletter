740ad8f1b7c928c9a5209e5ea125be6ce894078e
DanielWu1 <tony153265964@gmail.com>
2023-09-28 03:12:21 -0400

    add right controler

---
+++ my-app/src/subcomponents/generate/RightControl.js
@@ -0,0 +1,42 @@
+import { React, useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import { Button, Checkbox, Label, TextInput, ToggleSwitch, Textarea, Select as Select1 } from "flowbite-react";
+import { ThemeTopic } from "../../constants/ThemeTopic";
+
+
+function RightControl(props) {
+    let dispatch = useDispatch();
+    let content
+    if (props.select === "layOut") {
+        content = (
+            <Select1
+                value={props.firstPageData[2].data}
+                onChange={(e) => {
+                    let tem = JSON.parse(JSON.stringify(props.firstPageData));
+                    tem[2].data = e.target.value
+                    console.log("tem", tem);
+                    props.updateData(tem)
+                }}
+            >
+                <option disabled key="default" value=""></option>
+                {ThemeTopic.map((font, idx) => (
+                    <option key={idx} value={font} style={{ fontSize: font }}>
+                        {font}
+                    </option>
+                ))}
+            </Select1>
+        )
+    }
+
+    return (
+        <div className="w-full h-full flex flex-col overflow-y-scroll">
+            <div className="flex justify-center items-center w-full h-14">
+                <h3> Custom Style</h3>
+            </div>
+            <div className="w-full h-full">
+                {content}
+            </div>
+        </div>
+    )
+}
+export default RightControl;
