b1347a697dbe393114cacd1097972d7a892b1b61
Rajat Raghuwanshi <77507423+rajat10cube@users.noreply.github.com>
2023-09-28 14:35:20 -0400

    changed UI of navbar

--- my-app/src/components/MainNav.js
+++ my-app/src/components/MainNav.js
@@ -6,7 +6,7 @@ import { useNavigate } from "react-router-dom";
 import { accountPath, loginPagePath, mainPagePath, DetailPagePath, allnewsletter } from "../constants/RouteConstants";
 import { Dropdown, Navbar, Avatar, DarkThemeToggle } from "flowbite-react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCoins } from "@fortawesome/free-solid-svg-icons";
+import { faCoins, faPlus } from "@fortawesome/free-solid-svg-icons";
 import { Link, useLocation } from "react-router-dom";
 import { useDetailPageOne } from "../redux/DetailSlice"
 import { useUser, viewUser } from "../redux/UserSlice";
@@ -27,12 +27,13 @@ function MainNav(props) {
   }, []);
 
   return (
-    <Navbar className="bg-zinc-200 navbar-fixed h-[6%]" fluid rounded>
-      <Navbar.Brand href="https://anote.ai/">
+    // <Navbar className="bg-zinc-200 navbar-fixed h-[6%]" fluid rounded>
+    <Navbar fluid >
+      <Navbar.Brand href="/">
         {/* Image only when the theme is light  */}
-        <div className="h-10 w-10 bg-center bg-contain bg-[url('../public/logo_light.png')] dark:bg-[url('../public/logo_dark.png')]"></div>
+        {/* <div className="h-10 w-10 bg-center bg-contain bg-[url('../public/logo_light.png')] dark:bg-[url('../public/logo_dark.png')]"></div> */}
         <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white text-[#374151]">
-          Anote
+          nwsltr.ai
         </span>
       </Navbar.Brand>
       <div className="flex md:order-2">
@@ -43,6 +44,10 @@ function MainNav(props) {
             props.setDarkTheme(themeMode);
           }}
         /> */}
+        <div onClick={()=> navigate(mainPagePath)} className="cursor-pointer flex font-bold text-sm items-center bg-gray-900 text-white rounded-xl px-2 border border-gray-500 mr-5">
+          <FontAwesomeIcon icon={faPlus} className="mr-2" />
+          <p>Create New NewsLetter</p>
+        </div>
         <Dropdown
           theme={{
             arrowIcon: "text-[#374151] dark:text-[#9BA3AF] ml-2 h-4 w-4",
@@ -86,9 +91,9 @@ function MainNav(props) {
         <Navbar.Link href={allnewsletter}>
           <p>All Letters</p>
         </Navbar.Link>
-        <Navbar.Link href={mainPagePath}>
-          <p>Create</p>
-        </Navbar.Link>
+        {/* <Navbar.Link href={mainPagePath}>
+          <p>Create New</p>
+        </Navbar.Link> */}
       </Navbar.Collapse>
     </Navbar>
   );
